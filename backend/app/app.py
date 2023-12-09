01a5bdfb88ffab201516497f7882c100cf17d062
Red6Wolf <redwolf343666@gmail.com>
2023-12-09 14:10:23 +0300

    Propblem with settigns

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from app.core.config import settings
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
 
-app = FastAPI()
 
-@app.get('/')
-async def hello():
-    return {"message": "Hello, world"}
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+@app.on_event("startup")
+async def app_init():
+    """
+         initialize crucial application services
+    """
+    
+    db_client = AsyncIOMotorClient(settings.MONGO_CONNECTION_STIRING).todolist
+    
+    await init_beanie(
+        database=db_client,
+        document_models = [
+            
+        ]
+    )
+    
