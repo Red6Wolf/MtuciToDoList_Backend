544b6a1cf992d3840344a649679aee98eeb67d95
Red6Wolf <redwolf343666@gmail.com>
2023-12-15 23:53:52 +0300

    some fix

--- backend/app/api/api_v1/handlers/user.py
+++ backend/app/api/api_v1/handlers/user.py
@@ -10,9 +10,21 @@ user_router = APIRouter()
 @user_router.post('/create', summary="Create new user", response_model= UserOut)
 async def create_user(data: UserAuth):
     try:
+        if len(data.username) < 4:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Username should have at least 4 characters"
+            )
+            
+        if len(data.password) < 6:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Password should have at least 6 characters"
+            )
+            
         return await UserService.create_user(data)
     except pymongo.errors.DuplicateKeyError:
         raise HTTPException(
-            status_code = status.HTTP_400_BAD_REQUEST,
-            detail = "User already exist"      
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User already exists"
         )
